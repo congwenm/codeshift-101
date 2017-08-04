fa9efeff7c55cafc35c373c02e09736d830de11a
Congwen Ma <lostaznfound@gmail.com>
2017-08-04 12:55:58 -0500

    exercise 1 and 2

---
+++ basic/remove-consoles.js
@@ -0,0 +1,31 @@
+// run with 
+// -p displays result, tally of dispositions for each file processed
+// -d remove this, and it would replace the content in  input.js
+// jscodeshift -t remove-consoles.js input.js -d -p
+export const oldschool = (fileInfo, api) => {
+  const j = api.jscodeshift
+  const root = j(fileInfo.source)
+
+  const callExpressions = root.find(j.CallExpression, {
+    callee: {
+      type: 'MemberExpression',
+      object: { type: 'Identifier', name: 'console' }
+    }
+  })
+
+  callExpressions.remove() // remove the console
+
+  return root.toSource()
+}
+export default (fileinfo, api) => {
+  const j = api.jscodeshift
+  return j(fileinfo.source)
+    .find(j.CallExpression, {
+      callee: {
+        type: 'MemberExpression',
+        object: { type: 'Identifier', name: 'console' }
+      }
+    })
+    .remove()
+    .toSource()
+}
